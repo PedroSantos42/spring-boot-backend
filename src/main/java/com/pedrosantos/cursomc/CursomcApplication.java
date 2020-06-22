ad6f868a8d79e48bf383374c667ee963a758c32b
pedro.santos <pedroabs.dev42@gmail.com>
2020-06-22 01:30:29 -0300

    customers, addresses and telephones

--- src/main/java/com/pedrosantos/cursomc/CursomcApplication.java
+++ src/main/java/com/pedrosantos/cursomc/CursomcApplication.java
@@ -7,12 +7,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.pedrosantos.cursomc.domain.Address;
 import com.pedrosantos.cursomc.domain.Category;
 import com.pedrosantos.cursomc.domain.City;
+import com.pedrosantos.cursomc.domain.Customer;
 import com.pedrosantos.cursomc.domain.Product;
 import com.pedrosantos.cursomc.domain.State;
+import com.pedrosantos.cursomc.domain.enums.CustomerType;
+import com.pedrosantos.cursomc.repositories.AddressRepository;
 import com.pedrosantos.cursomc.repositories.CategoryRepository;
 import com.pedrosantos.cursomc.repositories.CityRepository;
+import com.pedrosantos.cursomc.repositories.CustomerRepository;
 import com.pedrosantos.cursomc.repositories.ProductRepository;
 import com.pedrosantos.cursomc.repositories.StateRepository;
 
@@ -31,9 +36,15 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private StateRepository stateRepository;
-	
+
 	@Autowired
 	private CityRepository cityRepository;
+
+	@Autowired
+	private CustomerRepository customerRepository;
+	
+	@Autowired
+	private AddressRepository addressRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -62,6 +73,18 @@ public class CursomcApplication implements CommandLineRunner {
 		state1.getCities().addAll(Arrays.asList(city1));
 		state2.getCities().addAll(Arrays.asList(city2, city3));
 		
+		
+		
+		// cliente
+		
+		Customer customer1 = new Customer(null, "Ana Paula", "ana@gmail.com", "39865644520", CustomerType.PERSON_PHYSICAL);
+		customer1.getTelephones().addAll(Arrays.asList("88569963", "99875263"));
+
+		Address address1 = new Address(null, "Rua Flores", "120", "APTO 201", "Jardins", "5962050", customer1, city1);
+		Address address2 = new Address(null, "Av Matos", "314", "Sala 800", "Centro", "62044230", customer1, city2);
+		
+		customer1.getAddresses().addAll(Arrays.asList(address1, address2));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		
 		productRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
@@ -69,5 +92,9 @@ public class CursomcApplication implements CommandLineRunner {
 		stateRepository.saveAll(Arrays.asList(state1, state2));
 		
 		cityRepository.saveAll(Arrays.asList(city1, city2, city3));
+		
+		customerRepository.saveAll(Arrays.asList(customer1));
+		
+		addressRepository.saveAll(Arrays.asList(address1, address1));
 	}
 }
---
+++ src/main/java/com/pedrosantos/cursomc/domain/enums/CustomerType.java
@@ -0,0 +1,38 @@
+package com.pedrosantos.cursomc.domain.enums;
+
+public enum CustomerType {
+
+	PERSON_PHYSICAL(1, "Pessoa Física"),
+	PERSON_JURIDICAL(2, "Pessoa Jurídica");
+	
+	private int cod;
+	private String description;
+	
+	private CustomerType(int cod, String description) {
+		this.cod = cod;
+		this.description = description;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	public static CustomerType toEnum(Integer cod) {
+		
+		if (cod == null) {
+			return null;
+		}
+		
+		for (CustomerType customer : CustomerType.values()) {
+			if (cod.equals(customer.getCod())) {
+				return customer;
+			}
+		}
+		
+		throw new IllegalArgumentException("ID inválido: " + cod);
+	}
+}
