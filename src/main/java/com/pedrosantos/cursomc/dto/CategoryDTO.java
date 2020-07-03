a1cc569e20c65771e02acfc35bc18998b66c4441
pedro.santos <pedroabs.dev42@gmail.com>
2020-07-03 16:02:05 -0300

    Bean Validation

--- src/main/java/com/pedrosantos/cursomc/dto/CategoryDTO.java
+++ src/main/java/com/pedrosantos/cursomc/dto/CategoryDTO.java
@@ -2,12 +2,18 @@ package com.pedrosantos.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
+
 import com.pedrosantos.cursomc.domain.Category;
 
 public class CategoryDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private Integer id;
+
+    @NotEmpty(message = "Preenchimento obrigatório")
+    @Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
     private String name;
 
     public CategoryDTO(Category c) {
--- src/main/java/com/pedrosantos/cursomc/service/CategoryService.java
+++ src/main/java/com/pedrosantos/cursomc/service/CategoryService.java
@@ -1,8 +1,9 @@
 package com.pedrosantos.cursomc.service;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -12,6 +13,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.pedrosantos.cursomc.domain.Category;
+import com.pedrosantos.cursomc.dto.CategoryDTO;
 import com.pedrosantos.cursomc.repositories.CategoryRepository;
 import com.pedrosantos.cursomc.service.exceptions.DataIntegrityException;
 import com.pedrosantos.cursomc.service.exceptions.ObjectNotFoundException;
@@ -26,7 +28,7 @@ public class CategoryService {
 		Optional<Category> obj = repo.findById(id);
 
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! ID: " + id + ", Tipo: " + Category.class.getName()));
+				"Objeto não encontrado! ID: " + id + ", Tipo: " + Category.class.getSimpleName()));
 	}
 
 	public Category insert(Category obj) {
@@ -54,7 +56,10 @@ public class CategoryService {
 
 	public Page<Category> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-
 		return repo.findAll(pageRequest);
+	}
+
+	public Category fromDTO(CategoryDTO objDto) {
+		return new Category(objDto.getId(), objDto.getName());
 	}
 }
